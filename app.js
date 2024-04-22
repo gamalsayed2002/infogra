f24a618b13f653d0009d24ea2e0f2361fb88cf70
gamal <gamalsayedccss@gmail.com>
2024-04-22 03:52:30 +0200

    creating landing page

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onscroll = function () {
+  if (scrollY > 10) {
+    let header = document.querySelector("header");
+
+    header.style.top = "0";
+    header.style.padding = "30px 0";
+    header.style.background = "rgba(0 0 0 / 70%)";
+    header.style.backdropFilter = "blur(5px)";
+  } else {
+    let header = document.querySelector("header");
+
+    header.style.padding = "40px 0";
+    header.style.background = "transparent";
+  }
+};
+// show nav
+let menu = document.querySelector(".container .menu");
+let ul = document.querySelector("header nav ul");
+let line_1 = document.querySelector("header nav .menu .line-1")
+console.log(line_1)
+let line_2 = document.querySelector("header nav .menu .line-2")
+console.log(line_2)
+let line_3 = document.querySelector("header nav .menu .line-3")
+console.log(line_3)
+
+menu.addEventListener("click", () => {
+  ul.classList.toggle("show");
+  line_1.classList.toggle("line-1-clicked")
+  line_2.classList.toggle("line-2-clicked")
+  line_3.classList.toggle("line-3-clicked")
+});

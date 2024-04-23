3a80a2ae51c793ec3eb7b98d5e816bb72dd4b38f
gamal <gamalsayedccss@gmail.com>
2024-04-23 07:41:34 +0200

    adding portfolio and services sections

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.onscroll = function () {
-  if (scrollY > 10) {
+  if (scrollY > 50) {
     let header = document.querySelector("header");
 
     header.style.top = "0";
@@ -16,16 +16,15 @@ window.onscroll = function () {
 // show nav
 let menu = document.querySelector(".container .menu");
 let ul = document.querySelector("header nav ul");
-let line_1 = document.querySelector("header nav .menu .line-1")
-console.log(line_1)
-let line_2 = document.querySelector("header nav .menu .line-2")
-console.log(line_2)
-let line_3 = document.querySelector("header nav .menu .line-3")
-console.log(line_3)
+let line_1 = document.querySelector("header nav .menu .line-1");
+let line_2 = document.querySelector("header nav .menu .line-2");
+let line_3 = document.querySelector("header nav .menu .line-3");
 
 menu.addEventListener("click", () => {
   ul.classList.toggle("show");
-  line_1.classList.toggle("line-1-clicked")
-  line_2.classList.toggle("line-2-clicked")
-  line_3.classList.toggle("line-3-clicked")
+  line_1.classList.toggle("line-1-clicked");
+  line_2.classList.toggle("line-2-clicked");
+  line_3.classList.toggle("line-3-clicked");
 });
+// start nav on media screen
+
